99618c121bea38ad86fd75cf8dba5e37791ab85d
Vili Mähönen <vilimahonen@icloud.com>
2025-12-09 22:43:57 +0200

    tehtäviä

--- viikko6/laskin/src/kayttoliittyma.py
+++ viikko6/laskin/src/kayttoliittyma.py
@@ -8,11 +8,60 @@ class Komento(Enum):
     NOLLAUS = 3
     KUMOA = 4
 
+class Summa:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+
+    def suorita(self):
+        try:
+            luku = int(self._lue_syote())
+            self._sovelluslogiikka.plus(luku)
+        except ValueError:
+            pass
+
+class Erotus:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+
+    def suorita(self):
+        try:
+            luku = int(self._lue_syote())
+            self._sovelluslogiikka.miinus(luku)
+        except ValueError:
+            pass
+
+class Nollaus:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+
+    def suorita(self):
+        self._sovelluslogiikka.nollaa()
+
+class Kumoa:
+    def __init__(self, sovelluslogiikka, lue_syote):
+        self._sovelluslogiikka = sovelluslogiikka
+        self._lue_syote = lue_syote
+
+    def suorita(self):
+        self._sovelluslogiikka.kumoa()
 
 class Kayttoliittyma:
     def __init__(self, sovelluslogiikka, root):
         self._sovelluslogiikka = sovelluslogiikka
         self._root = root
+
+        self._komennot = {
+            Komento.SUMMA: Summa(sovelluslogiikka, self._lue_syote),
+            Komento.EROTUS: Erotus(sovelluslogiikka, self._lue_syote),
+            Komento.NOLLAUS: Nollaus(sovelluslogiikka, self._lue_syote),
+            Komento.KUMOA: Kumoa(sovelluslogiikka, self._lue_syote)
+        }
+
+    def _lue_syote(self):
+        return self._syote_kentta.get()
 
     def kaynnista(self):
         self._arvo_var = StringVar()
@@ -55,21 +104,8 @@ class Kayttoliittyma:
         self._kumoa_painike.grid(row=2, column=3)
 
     def _suorita_komento(self, komento):
-        arvo = 0
-
-        try:
-            arvo = int(self._syote_kentta.get())
-        except Exception:
-            pass
-
-        if komento == Komento.SUMMA:
-            self._sovelluslogiikka.plus(arvo)
-        elif komento == Komento.EROTUS:
-            self._sovelluslogiikka.miinus(arvo)
-        elif komento == Komento.NOLLAUS:
-            self._sovelluslogiikka.nollaa()
-        elif komento == Komento.KUMOA:
-            pass
+        komento_olio = self._komennot[komento]
+        komento_olio.suorita()
 
         self._kumoa_painike["state"] = constants.NORMAL
 
--- viikko6/laskin/src/sovelluslogiikka.py
+++ viikko6/laskin/src/sovelluslogiikka.py
@@ -1,15 +1,24 @@
 class Sovelluslogiikka:
     def __init__(self, arvo=0):
         self._arvo = arvo
+        self._historia = []
 
     def miinus(self, operandi):
+        self._historia.append(self._arvo)
         self._arvo = self._arvo - operandi
 
     def plus(self, operandi):
+        self._historia.append(self._arvo)
         self._arvo = self._arvo + operandi
 
     def nollaa(self):
+        self._historia.append(self._arvo)
         self._arvo = 0
+
+    def kumoa(self):
+        """Palauta edelliseen arvoon historiasta."""
+        if self._historia:
+            self._arvo = self._historia.pop()
 
     def aseta_arvo(self, arvo):
         self._arvo = arvo
